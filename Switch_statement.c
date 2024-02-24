6252df8539b7329a7380943b7de4c1d1bb78a879
Vibha Pateshwari <pateshwari.vibha@gmail.com>
2024-02-24 17:12:00 +0530

    Add files via upload

---
+++ Switch_statement.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number between 1 to 7: ");
+    scanf("%d", &number);
+    switch(number) {
+        case 1:
+        printf("Sunday");
+        break;
+        
+        case 2:
+        printf("Monday");
+        break;
+        
+        case 3:
+        printf("Tuesday");
+        break;
+        
+        case 4:
+        printf("Wednesday");
+        break;
+        
+        case 5:
+        printf("Thrusday");
+        break;
+        
+         case 6:
+        printf("Friday");
+        break;
+        
+        case 7:
+        printf("Saturday");
+        break;
+        
+        default:
+        printf("Invalid Number");
+        
+    }
+    return 0;
+}
